df50321571ebe793f6fec8da1396b9d1a6fbc7fa
Elen Eisendle <ln@calcifer.ee>
2020-07-16 21:52:05 +0200

    Add rotation fix support

--- __init__.py
+++ __init__.py
@@ -1,11 +1,36 @@
 import csv
 import os
 import pcbnew
+import re
 import wx
 from decimal import Decimal
 from pathlib import Path
 
 ref_ignore = ["TP", "J", "NT", "SW"]
+
+# original rotation db from:
+# https://github.com/matthewlai/JLCKicadTools/blob/master/jlc_kicad_tools/cpl_rotations_db.csv
+rotations = {
+    "^SOT-223": 180,
+    "^SOT-23": 180,
+    "^SOT-353": 180,
+    "^QFN-": 270,
+    "^LQFP-": 90,
+    "^TQFP-": 270,
+    "^SOP-(?!18_)": 270,
+    "^TSSOP-": 270,
+    "^DFN-": 270,
+    "^SOIC-": 90,
+    "^SOP-18_": 0,
+    "^VSSOP-10_": 270,
+    "^CP_EIA-3216-18_": 180,
+    "^CP_Elec_8x10.5": 180,
+    "^CP_Elec_6.3x7.7": 180,
+    "^CP_Elec_8x6.7": 180,
+    "^(.*?_|V)?QFN-(16|20|24|28|40)(-|_|$)": 270,
+    "^MSOP-10_": 90,
+    "^R_Array_Convex_4x0603": 90,
+}
 
 class JLCSMTPlugin(pcbnew.ActionPlugin):
     def defaults(self):
@@ -13,7 +38,8 @@ class JLCSMTPlugin(pcbnew.ActionPlugin):
         self.category = "Fabrication Outputs"
         self.description = "Generates the CPL placement files as expected by JLCSMT"
         self.show_toolbar_button = True
-        self.icon_file_name = os.path.join(os.path.dirname(__file__), 'KiJLC_32x32.png')
+        self.icon_file_name = os.path.join(
+            os.path.dirname(__file__), 'KiJLC_32x32.png')
 
     def Run(self):
         board = pcbnew.GetBoard()
@@ -21,12 +47,14 @@ class JLCSMTPlugin(pcbnew.ActionPlugin):
 
         fn = Path(board.GetFileName()).with_suffix("")
 
-        bot = open("{}_bot.cpl".format(fn), "w", newline='')
-        top = open("{}_top.cpl".format(fn), "w", newline='')
-        botw = csv.writer(bot, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-        topw = csv.writer(top, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        bot = open("{}_cpl_bot.csv".format(fn), "w", newline='')
+        top = open("{}_cpl_top.csv".format(fn), "w", newline='')
+        botw = csv.writer(bot, delimiter=',', quotechar='"',
+                          quoting=csv.QUOTE_ALL)
+        topw = csv.writer(top, delimiter=',', quotechar='"',
+                          quoting=csv.QUOTE_ALL)
 
-        hdr = ["Ref","PosX","PosY","Rot","Side"]
+        hdr = ["Designator", "Mid X", "Mid Y", "Layer", "Rotation"]
         botw.writerow(hdr)
         topw.writerow(hdr)
 
@@ -45,18 +73,28 @@ class JLCSMTPlugin(pcbnew.ActionPlugin):
             rot = mod.GetOrientationDegrees()
             desc = mod.GetDescription()
             layer = board.GetLayerName(mod.GetLayer())
-            mid_x = Decimal(pos[0]) / Decimal(100000)
-            mid_y = Decimal(pos[1]) / Decimal(100000)
+            mid_x = str(Decimal(pos[0]) / Decimal(1000000)) + "mm"
+            mid_y = str(Decimal(pos[1]) / Decimal(-1000000)) + "mm"
+            footprint = str(mod.GetFPID().GetLibItemName())
+
+
+            for exp in rotations:
+                if re.match(exp, footprint):
+                    new_rot =  (rot + rotations[exp]) % 360
+                    print("rotating {} ({}): prev {}, new {}".format(ref, footprint, rot, new_rot))
+                    rot = new_rot
 
             if layer == "F.Cu":
                 layer = "top"
-                topw.writerow([ref, mid_x, mid_y, rot, layer])
+                topw.writerow([ref, mid_x, mid_y, layer, rot])
             elif layer == "B.Cu":
                 layer = "bottom"
-                botw.writerow([ref, mid_x, mid_y, rot, layer])
-        
+                botw.writerow([ref, mid_x, mid_y, layer, rot])
+
         bot.close()
         top.close()
-        wx.MessageBox("Placement files generated.", 'Done', wx.OK | wx.ICON_INFORMATION)
+        wx.MessageBox("Placement files generated.",
+                      'Done', wx.OK | wx.ICON_INFORMATION)
+
 
 JLCSMTPlugin().register()
