3ea66e0b32247fd1abd933aa4ac14d6195ca8fe9
Egil Kvaleberg <egil@kvaleberg.no>
2021-12-29 03:02:48 +0100

    Uodate to latest KiCAD 6.0 release. GetModules() is depreceated, and no longer available.

--- __init__.py
+++ __init__.py
@@ -134,7 +134,7 @@ class JLCSMTPlugin(pcbnew.ActionPlugin):
 
     def Run(self):
         board = pcbnew.GetBoard()
-        modules = board.GetModules()
+        modules = board.GetFootprints() # was GetModules() but this does not work with KiCAD 6.0
 
         fn = Path(board.GetFileName()).with_suffix("")
 
